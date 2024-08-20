f951aa542e2bad94be98a2d73480e8b482129447
Warda-Tahir <wardatahir907@gmail.com>
2024-08-21 02:03:40 +0500

    Deploying to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Header from './Components/Header/header'
 import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
 
 const App = () => {
   return (
     <div>
       <Header />
       <Navbar />
+      <Home/>
       <Footer/>
     </div>
   )
